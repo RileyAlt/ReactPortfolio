4107474cbe23167bb4982e14c6e472a20f123d17
RileyAlt <rileyaltenburg@gmail.com>
2023-05-10 16:03:58 -0500

    simplification

---
+++ react-mod/src/components/AboutMe.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const AboutMe = () => {
+    return (
+        <p> iam on the about me page</p>
+    )
+}
+
+export default AboutMe;
--- react-mod/src/index.js
+++ react-mod/src/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <p> I am the header </p>
+      <Routes>
+        <Route path="/" element={<p>I am at the root </p>} />
+        <Route path="/contact" element={<p>I am on the contact me page</p>} />
+        <Route path="/resume" element={<p>I am on the resume page</p>} />
+      </Routes>
+      {/* 
+        <Routes>
+          <Route path="/" element={<AboutMe/>} />
+          <Route path="/about-me" element={<AboutMe/>} />
+          <Route path="/contact" element={<ContactForm/>} />
+          <Route path="/resume" element={<Resume/>} />
+        </Routes>
+      */}
+      <p> I am the footer </p>
+      <Link to={'/contact'}>Go to contact form</Link>
+    </Router>
   </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
